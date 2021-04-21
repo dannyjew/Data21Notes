41377a4faa20983889c36ae8ca2c992fa534fdd0
dannyjew <djew@spartaglobal.com>
2021-04-21 17:31:41 +0100

    created hang man game

---
+++ InvokePractice.py
@@ -0,0 +1,8 @@
+from Numbers import num_list
+
+for x in num_list:
+    print(str(x))
+
+import random
+
+print(random.randint(1, 20))
--- variables.py
+++ variables.py
@@ -2,3 +2,19 @@ def doublefunction(x: int):
     return x*2
 
 
+def fizzbuzz(divisor1, divisor2, max_number: int):
+    result = []
+    for number in range(1, max_number):
+        if number % divisor1 == 0 and number % divisor2 == 0:
+            result.append("FizzBuzz "+str(number))
+        else:
+            continue
+    return result
+
+
+for x in fizzbuzz(3, 5, 20):
+    print(x)
+
+
+
+
